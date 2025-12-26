8ef5dc30811399e8020d17378f0efae003436cb2
charles <km1104rs@gmail.com>
2025-12-26 11:05:29 +0900

    fix: datasource 참조 제거 (prod 프로파일에서 가져오도록)

--- src/main/java/com/baektracker/global/config/DatabaseConfig.java
+++ src/main/java/com/baektracker/global/config/DatabaseConfig.java
@@ -2,8 +2,6 @@ package com.baektracker.global.config;
 
 import jakarta.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -14,33 +12,9 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class DatabaseConfig {
-    @Value("${spring.datasource.driver-class-name}")
-    public String driverClassName;
-
-    @Value("${spring.datasource.url}")
-    public String url;
-
-    @Value("${spring.datasource.username}")
-    public String username;
-
-    @Value("${spring.datasource.password}")
-    public String password;
-
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        // Mybatis Debuging
-        dataSourceBuilder.driverClassName(driverClassName);
-        dataSourceBuilder.url(url);
-        dataSourceBuilder.username(username);
-        dataSourceBuilder.password(password);
-
-        return dataSourceBuilder.build();
     }
 
     @Bean
@@ -50,7 +24,7 @@ public class DatabaseConfig {
     }
 
     @Bean(name = "mybatisTxManager")
-    public PlatformTransactionManager mybatisTxManager() {
-        return new DataSourceTransactionManager(dataSource());
+    public PlatformTransactionManager mybatisTxManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
     }
 }
